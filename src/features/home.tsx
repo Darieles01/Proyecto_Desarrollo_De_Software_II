9394d997ddc0434b9ab8225867f4606021192b5a
Venticio <viniciocastro0@gmail.com>
2026-02-16 18:53:52 -0600

    Botones en home y header para navegar en la web

--- src/features/home.tsx
+++ src/features/home.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { clients, pets } from "../data/mockdata";
 
 export default function Home() {
@@ -6,6 +7,14 @@ export default function Home() {
       <h1>Welcome to the Veterinary Clinic</h1>
       <p>Client: {clients[0].name}</p>
       <p>Total pets: {pets.length}</p>
+      <div>
+        <Link to="/mis-mascotas">
+          <button>Mis Mascotas</button>
+        </Link>
+        <Link to="/perfil-cliente">
+          <button>Perfil del Cliente</button>
+        </Link>
+      </div>
     </div>
   );
 }
