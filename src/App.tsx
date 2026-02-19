f33102a2991d9846d76b954c5aa5ae9931479f08
Arwy Zam <arwenoras12@gmail.com>
2026-02-18 20:31:34 -0600

    Arreglado acceso de PetProfile

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import NotFound from "./shared/NotFound";
 import Home from "./features/Home/Home";
 import { Profile } from "./features/Customers_Profile/Customer_Profile";
 import PetList from "./features/Pets_List/Pet_List";
+import { PetProfile } from "./features/Home/PetProfile/PetProfile";
 
 export default function App() {
   return (
@@ -17,6 +18,7 @@ export default function App() {
             <Route path="/" element={<Home />} />
             <Route path="/profile" element={<Profile />} />
             <Route path="/pets" element={<PetList />} />
+            <Route path="/pet/:id" element={<PetProfile />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
